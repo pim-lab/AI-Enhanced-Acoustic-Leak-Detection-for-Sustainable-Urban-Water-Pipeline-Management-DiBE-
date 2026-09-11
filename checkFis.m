ea6e24379f4e5d59f9e65924813652bc5f71b6d7
DaniNugraha15 <dani301526@gmail.com>
2026-09-11 13:20:20 +0800

    Add files via upload

---
+++ checkFis.m
@@ -0,0 +1,5 @@
+fis = readfis('FAATOv4.fis');  % .fis file is fuzzy interface system
+showrule(fis)
+
+% I made this so that we can see the Fuzzy Interface Rule that have been
+% implemented in FAATO (Fuzzy Adaptive Artic-Tern Optimization).
